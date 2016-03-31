e5e38cec217fa1ddb25c2c2b97838fc9a6e7788f
x-G-h-o-s-t-x <ghostx@ntlworld.com>
2016-03-31 04:56:16 +0100

    new bind function to accept arrays
    now uses arrays.
    $placeholders = array('test 1' => 1, 'test 2' => '2', 'test 3' => true);
    db::pdo()->bind($placeholders);

--- pdo.class.php
+++ pdo.class.php
@@ -54,23 +54,26 @@ class db {
 
     // binds values to placeholders
     // http://php.net/manual/en/pdostatement.bindvalue.php
-    public function bind($param, $value, $type = null) {
-        if(is_null($type)):
-            switch(true) {
-                case is_int($value):
-                    $type = PDO::PARAM_INT;
-                    break;
-                case is_bool($value):
-                    $type = PDO::PARAM_BOOL;
-                    break;
-                case is_null($value):
-                    $type = PDO::PARAM_NULL;
-                    break;
-                default:
-                    $type = PDO::PARAM_STR;
-            }
-        endif;
-        $this->query->bindValue($param, $value, $type);
+    public function bind($arr = array()) {
+        foreach($arr as $param => $value):
+            $type = null;
+                if(is_null($type)):
+                    switch(true) {
+                        case is_int($value):
+                            $type = PDO::PARAM_INT;
+                            break;
+                        case is_bool($value):
+                            $type = PDO::PARAM_BOOL;
+                            break;
+                        case is_null($value):
+                            $type = PDO::PARAM_NULL;
+                            break;
+                        default:
+                            $type = PDO::PARAM_STR;
+                    }
+                endif;
+            $this->query->bindValue($param, $value, $type);
+        endforeach;
     }
 
     // executes the pdo query
